75345c9a41a9a0eeb05a5d65f3e3ceb29979da0d
Scorupa <majok.scorupa@gmail.com>
2025-02-22 15:10:21 +0900

    Web page accepts user text input

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-# React + Vite
+### Running project locally
+
+`npm run dev`
+
+---
+
+### (memo) React + Vite
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,31 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [text, setText] = useState('')
+  const [displayText, setDisplayText] = useState('')
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setDisplayText(text);
+  };
+
+  const handleChange = e => {
+    setText(e.target.value);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <h1>Harmoi</h1>
+
+      <form onSubmit={e => handleSubmit(e)}>
+        <h3>Insert text to see Harmoi character </h3>
+        <input type="text" placeholder="Insert text here" value={text} onChange={e => handleChange(e)} />
+        <button type="submit" onClick={handleSubmit}>
+          Submit
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+        {<p>{displayText}</p>}
+      </form>
     </>
   )
 }
