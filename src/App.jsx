f2cb9db00e8949293b54926fe713ece8953576a5
Scorupa <majok.scorupa@gmail.com>
2025-02-23 19:52:53 +0900

    list now displays characters as well

--- src/App.jsx
+++ src/App.jsx
@@ -1,105 +1,84 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
-// Dynamically import SVGs using import.meta.glob()
-const svgFolderPath = '../harmoiGlyphs/';
+// Dynamically import SVGs
 const svgModules = import.meta.glob('../harmoiGlyphs/*.svg');
 
 function App() {
-  // ---- State variables and setters ------------------------------------
   const [searchText, setSearchText] = useState('');
   const [submittedText, setSubmittedText] = useState('');
-
-  const [SelectedSvg, setSelectedSvg] = useState(null);
+  const [selectedSvg, setSelectedSvg] = useState(null);
 
   const [isListOpen, setIsListOpen] = useState(false);
-  const [svgNames, setSvgNames] = useState([]);
-
-  // ---- functions ------------------------------------------------------
+  const [svgData, setSvgData] = useState([]);
 
   useEffect(() => {
-    // Extract file names from the paths of SVGs to show as options
-    setSvgNames(Object.keys(svgModules).map((path) =>
-      path.replace(svgFolderPath, '')
-          .replace('.svg', '')));
-  }, []);
+    // Load SVG names and their previews
+    const loadSVGs = async () => {
+      const entries = await Promise.all(
+        Object.entries(svgModules).map(async ([path, importer]) => {
+          const name = path.split('/').pop().replace('.svg', '');
+          const module = await importer();
+          return { name, src: module.default || module.ReactComponent };
+        })
+      );
+      setSvgData(entries);
+    };
 
-  const handleInputChange = (e) => {
-    setSearchText(e.target.value);
-  };
+    loadSVGs();
+  }, []);
 
   const handleSearch = () => {
     setSubmittedText(searchText);
-    const svgPath = `${svgFolderPath}${searchText}.svg`;
-
-    if (svgModules[svgPath]) {
-      svgModules[svgPath]().then((module) => {
-        if (module.ReactComponent) {
-          // If the SVG is an inline component
-          setSelectedSvg(() => module.ReactComponent);
-        } else if (module.default) {
-          // If the SVG is an image URL
-          setSelectedSvg(module.default);
-        } else {
-          setSelectedSvg(null);
-        }
-      });
-    } else {
-      setSelectedSvg(null);
-    }
+    const foundSvg = svgData.find((svg) => svg.name === searchText);
+    setSelectedSvg(foundSvg ? foundSvg.src : null);
   };
-
-  // ---- html ------------------------------------------------------
 
   return (
     <>
       <h1>Harmoi Character Dictionary</h1>
 
-      {/* Input field for user to choose a Harmoi character */}
       <input
         type="text"
         value={searchText}
-        onChange={handleInputChange}
+        onChange={(e) => setSearchText(e.target.value)}
         placeholder="Enter a word (try 'yellow')"
       />
       <button onClick={handleSearch}>Search</button>
 
-      {/* Collapsible List of Available SVG Names */}
-      {svgNames.length > 0 && (
+      {/* Collapsible List of Available SVG Names with Previews */}
+      {svgData.length > 0 && (
         <div>
           <button onClick={() => setIsListOpen(!isListOpen)}>
             {isListOpen ? 'Hide List' : 'Show Characters List'}
           </button>
 
           {isListOpen && (
-            <div>
-              <h3>Available Harmoi Characters:</h3>
-              <ul>
-                {svgNames.map((word) => (
-                  <li key={word}>{word}</li>
-                ))}
-              </ul>
-            </div>
+            <ul>
+              {svgData.map(({ name, src }) => (
+                <li key={name} style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
+                  {typeof src === 'string' ? (
+                    <img src={src} alt={name} width={50} height={50} />
+                  ) : (
+                    <src width={50} height={50} />
+                  )}
+                  {name}
+                </li>
+              ))}
+            </ul>
           )}
         </div>
       )}
 
       {/* Render the selected SVG */}
-      {SelectedSvg &&
-        (typeof SelectedSvg === 'string' ? (
-          <div>
-            <h3>Selected SVG:</h3>
-            <img src={SelectedSvg} alt={searchText} width={100} height={100} />
-          </div>
+      {selectedSvg &&
+        (typeof selectedSvg === 'string' ? (
+          <img src={selectedSvg} alt={searchText} width={100} height={100} />
         ) : (
-          <div>
-            <h3>Selected SVG:</h3>
-            <SelectedSvg width={100} height={100} />
-          </div>
+          <selectedSvg width={100} height={100} />
         ))}
 
-      {/* Show message if no matching SVG is found */}
-      {!SelectedSvg && submittedText && <p>{submittedText} does not have a character.</p>}
+      {!selectedSvg && submittedText && <p>{submittedText} does not have a character.</p>}
     </>
   );
 }
